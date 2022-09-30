f72f28e44aa4795c1474d2f93b79f5abdf8131f2
i6rahim <i6rahim210@gmail.com>
2022-09-30 20:29:43 +0300

    "add"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,21 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
-
-/*=============== SWIPER POPULAR ===============*/
-
-
-/*=============== VALUE ACCORDION ===============*/
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+  const swiper = new Swiper('.swiper-web', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    wrapperClass: 'swiper-wrapper-web',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+ 
+  })
