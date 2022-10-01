95df4f751324eaca109e30ea3349891ac378e775
i6rahim <i6rahim210@gmail.com>
2022-10-01 19:14:42 +0300

    "add"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js'
 
-  const swiper = new Swiper('.swiper-web', {
+  const swiperWeb = new Swiper('.swiper-web', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 3,
@@ -17,5 +17,61 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.brow
       prevEl: '.swiper-button-prev',
     },
   
+ 
+  })
+
+  const swiperGraph = new Swiper('.swiper-graph', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    wrapperClass: 'swiper-wrapper-graph',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next-graph',
+      prevEl: '.swiper-button-prev-graph',
+    },
+  
+ 
+  })
+
+  const swiperService = new Swiper('.swiper-service', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    wrapperClass: 'swiper-wrapper-service',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.service-btn-next',
+      prevEl: '.service-btn-prev',
+    },
+ 
+  })
+
+  const swiperServiceGraph = new Swiper('.swiper-service_graph', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    wrapperClass: 'swiper-wrapper-service_graph',
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.graph-btn-next',
+      prevEl: '.graph-btn-prev',
+    },
  
   })
